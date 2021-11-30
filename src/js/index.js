f42a6a94db1c8e9d7a60b38cf6e47f4cbfdf17d7
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-11-30 19:17:15 +0600

    Add markup and styles for the To Do List

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,47 @@
 import '../styles/main.css';
 
-console.log('Wag1');
+const toDoListEl = document.getElementById('to-do-list');
+const addForm = document.getElementById('add-form');
+
+const tasks = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'complete To Do List project',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'shower',
+    completed: true,
+    index: 3,
+  },
+];
+
+function displayList() {
+  tasks.forEach((task) => {
+    const markup = `
+        <li class="main-list__item ${
+          task.completed ? 'main-list__item--checked' : ''
+        }">
+            
+            <label for="to-do-${task.index}"><input type="checkbox" ${
+      task.completed ? 'checked' : ''
+    } class='main-list__checkbox' name="to-do-${task.index}" id="to-do-${
+      task.index
+    }" />${task.description}</label>
+    <button class="btn btn-action" type="button"><ion-icon name="ellipsis-vertical-outline"></ion-icon></button>
+        </li> 
+        `;
+    toDoListEl.insertAdjacentHTML('beforeend', markup);
+  });
+}
+
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+displayList();
